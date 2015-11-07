4760775cbeb0fab5fbf91cb11853e75f57b373a7
Rich Abdill <richard.abdill@gmail.com>
2015-11-07 16:13:59 -0600

    City has update method now, am I smart?

--- backend/models/city.js
+++ backend/models/city.js
@@ -9,10 +9,30 @@ var citySchema = new Schema({
 	},
 	coin : {
 		count: { type : Number, default: 0 },
-		ratePerCapita: { type : Number, default: 1 }
+		ratePerCapita: { type : Number, default: 0.1 }
+	},
+	buildings : {
+		houses : [
+			{level: Number}
+		]
 	},
 	last_updated : { type : Date, default: Date.now }
 });
+
+citySchema.methods.update = function(callback) {
+	var now = new Date().getTime();
+	var last = new Date(this.last_updated).getTime();
+
+	// update tax revenue before population gets updated:
+	this.coin.count += this.coin.ratePerCapita * this.population.count;
+
+	// update population:
+	this.population.count += ((now - last)/1000) * this.population.rate;
+	this.last_updated = now;
+
+	// write it back to DB
+	return this.model('city').findByIdAndUpdate(this.id, { $set: this }, callback);
+};
 
 module.exports = {
 	City: mongoose.model('city', citySchema)
--- backend/routes/city.js
+++ backend/routes/city.js
@@ -1,18 +1,5 @@
 var City = require('../models/city').City;
 
-var update = function(city) {
-	var now = new Date().getTime();
-	var last = new Date(city.last_updated).getTime();
-
-	// update tax revenue before population gets updated:
-	city.coin.count += city.coin.ratePerCapita * city.population.count;
-
-	// update population:
-	city.population.count += ((now - last)/1000) * city.population.rate;
-	city.last_updated = now;
-	return city;
-};
-
 exports.square = function(req, res) {
 	City.findOne({ name: req.params.city }, function(err, city) {
 		if(err) {
@@ -23,8 +10,7 @@ exports.square = function(req, res) {
 				res.json(404, { message: "No city called " + req.params.city });
 			}
 			else {
-				city = update(city);
-				City.findByIdAndUpdate(city.id, { $set: city }, function (err, data) {
+				city.update(function (err, data) {
 					if(err) res.json(500, { message: err });
 					else {
 						res.json(200, city);
