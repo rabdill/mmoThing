7d0fe5a3cf989007ed3ee81926adde4f5c8295e3
Rich Abdill <richard.abdill@gmail.com>
2015-11-08 17:02:49 -0600

    Rounding displayed stats

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -17,6 +17,11 @@ mmoControllers.controller('HomeCtrl', ['$scope', "$q", "$interval", 'CitySvc', '
 	var updater = $interval(function() {
 		CitySvc.getStats().then(function(data) {
 			$scope.city = data;
+
+			$scope.city.coin.count = Math.floor($scope.city.coin.count);
+			$scope.city.food.count = Math.floor($scope.city.food.count);
+			$scope.city.coin.net = Math.floor(100 * ($scope.city.coin.ratePerCapita * $scope.city.population.count)) / 100;
+			$scope.city.food.net = Math.floor(100 * ($scope.city.food.rate - ($scope.city.food.consumptionPerCapita * $scope.city.population.count))) / 100;
 		});
 		console.log("Fetching...");
 	}, 3050);
